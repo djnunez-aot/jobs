56ffaee965baa480fc33ac488d6fbd036bd6e4c5
djnunez-aot <david.nunez@aot-technologies.com>
2023-07-16 15:38:47 -0400

    update requirements.txt

--- scraper-backend/api/init.py
+++ scraper-backend/api/init.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from .routes import my_route_function
+from .routes import scrape
 
 api_blueprint = Blueprint('api', __name__)
-api_blueprint.route('/my_route', methods=['GET', 'POST'])(my_route_function)
+api_blueprint.route('/scrape', methods=['POST'])(scrape)
