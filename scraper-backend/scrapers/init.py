17199a1e255e607b95d2db8518afc6f148ed4160
Dsoesito <99083937+Dsoesito@users.noreply.github.com>
2023-07-17 17:54:28 -0400

    used bool sets to simplify for loops

--- scraper-backend/scrapers/init.py
+++ scraper-backend/scrapers/init.py
@@ -7,8 +7,6 @@ from pprint import pprint
 
 def scrape_jobs(location, job_title):
     # Call the scrape function or any other scrapers here
-    results = scrape_realpython(location, job_title) 
+    results = scrape_realpython(location, job_title)
     # return results
     print(results)
-
-
